36dd3d4cd61be53db6e126e701768fbce3a28462
Carsten Mielke <carsten.mielke@kartverket.no>
2022-03-31 09:07:32 +0200

    lagt til basic stuff og bilder og noen styles

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+function App(): any {
+  return (
+    <div className="App">
+      <div className="logo-overlay">
+        <a className="logo-kartverket" href="https://kartverket.no/">kartverket.no</a>
+      </div>
+      <h1>Kart</h1>
+    </div>
+  );
+}
+
+export default App;
