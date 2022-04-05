4f6d526095e800e25b340ca15d37ec1007b79ad6
Amund Nedland <Amund.Nedland@norconsult.com>
2022-04-05 12:57:39 +0200

    Start adding languages

---
+++ src/i18n.ts
@@ -0,0 +1,47 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+i18n
+  // detect user language
+  // learn more: https://github.com/i18next/i18next-browser-languageDetector
+  .use(LanguageDetector)
+  // pass the i18n instance to react-i18next.
+  .use(initReactI18next)
+  // init i18next
+  // for all options read: https://www.i18next.com/overview/configuration-options
+  .init({
+    debug: true,
+    fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false, // not needed for react as it escapes by default
+    },
+    resources: {
+      en: {
+        translation: {
+          title: 'Norwaymap',
+          description: {
+            part1: 'Something in English',
+          }
+        }
+      },
+      no: {
+        translation: {
+          title: 'Norgeskart',
+          description: {
+            part1: 'Noe på norsk',
+          }
+        }
+      },
+      nn: {
+        translation: {
+          title: 'Noregs kart',
+          description: {
+            part1: 'Noe på nynorsk',
+          }
+        }
+      }
+    }
+  });
+
+export default i18n;
