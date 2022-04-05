b82064f7ef9551152bd4d2a7fd327cbb09370618
Knud S. Knudsen <Knud.Knudsen@norconsult.com>
2022-04-05 12:10:52 +0200

    code outside divs

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import MainMap from './components/MainMap';
+import SearchInput from './components/search/SearchInput';
 
 function App() {
   return (
     <div className="App">
+      <h1>Norgeskart</h1>
+      <SearchInput />
       <MainMap />
     </div>
   );
--- src/components/MainMap.tsx
+++ src/components/MainMap.tsx
@@ -11,9 +11,8 @@ import Map from '../MapLib/Map';
 import { osm, vector } from '../MapLib/Source';
 import wmts from '../MapLib/Source/wmts';
 import wmtsTileGrid from '../MapLib/TileGrid/wmts';
-import addMarkers from './Markers';
-import SearchInput from './search/SearchInput';
 import Logo from './Logo';
+import addMarkers from './Markers';
 
 const geojsonObject = mapConfig.geojsonObject;
 const geojsonObject2 = mapConfig.geojsonObject2;
@@ -25,7 +24,7 @@ export default function OverLayLayer() {
 
   const [showLayer1, setShowLayer1] = useState(true);
   const [showLayer2, setShowLayer2] = useState(true);
-  const [showWmts, setShowWmts] = useState(true);
+  const [showWmts, setShowWmts] = useState({ checked: true, wmtsLabel: 'norges_grunnkart' });
   const [showOsm, setShowOsm] = useState(false);
   const [showMarker, setShowMarker] = useState(false);
   const [wmtsLayer, setWmtsLayer] = useState('norges_grunnkart');
@@ -69,70 +68,81 @@ export default function OverLayLayer() {
     matrixIds: matrixIds,
   });
 
+  const handleCheckboxOsm = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    return setShowOsm(event.target.checked);
+  };
+  const handleCheckboxWmts = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    return setShowWmts({ checked: event.target.checked, wmtsLabel: 'norges_grunnkart' });
+  };
+  const handleSetWmtsLayer = (event: React.ChangeEvent<HTMLSelectElement>): void => {
+    setWmtsLayer(event.target.value);
+  };
+  const handleShowLayer1 = (event: React.ChangeEvent<HTMLInputElement>): void => setShowLayer1(event.target.checked);
+  const handleShowLayer2 = (event: React.ChangeEvent<HTMLInputElement>): void => setShowLayer2(event.target.checked);
+  const handleShowMarkers = (event: React.ChangeEvent<HTMLInputElement>): void => setShowMarker(event.target.checked);
+
+  const layerSelection = (
+    <Layers>
+      {showOsm && <TileLayer source={osm()} zIndex={0} />}
+      {showLayer1 && (
+        <VectorLayer
+          source={vector({
+            features: new GeoJSON().readFeatures(geojsonObject, {
+              featureProjection: get(sProjection) || undefined,
+            }),
+          })}
+          style={FeatureStyles.MultiPolygon}
+          zIndex={0}
+        />
+      )}
+      {showLayer2 && (
+        <VectorLayer
+          source={vector({
+            features: new GeoJSON().readFeatures(geojsonObject2, {
+              featureProjection: get(sProjection) || undefined,
+            }),
+          })}
+          style={FeatureStyles.MultiPolygon}
+          zIndex={0}
+        />
+      )}
+      {showWmts && (
+        <TileLayer
+          source={wmts({
+            url: 'http://opencache.statkart.no/gatekeeper/gk/gk.open_wmts?',
+            layer: wmtsLayer,
+            matrixSet: sProjection,
+            projection: projection,
+            tileGrid: tileGrid,
+            style: 'default',
+            format: 'image/png',
+          })}
+          zIndex={0}
+        />
+      )}
+      {showMarker && <VectorLayer source={vector({ features })} style={FeatureStyles.MultiPolygon} zIndex={0} />}
+    </Layers>
+  );
+
   return (
     <>
       <Map center={fromLonLat(center)} zoom={zoom}>
-        <Layers>
-          {showOsm && <TileLayer source={osm()} zIndex={0} />}
-          {showLayer1 && (
-            <VectorLayer
-              source={vector({
-                features: new GeoJSON().readFeatures(geojsonObject, {
-                  featureProjection: get(sProjection) || undefined,
-                }),
-              })}
-              style={FeatureStyles.MultiPolygon}
-              zIndex={0}
-            />
-          )}
-          {showLayer2 && (
-            <VectorLayer
-              source={vector({
-                features: new GeoJSON().readFeatures(geojsonObject2, {
-                  featureProjection: get(sProjection) || undefined,
-                }),
-              })}
-              style={FeatureStyles.MultiPolygon}
-              zIndex={0}
-            />
-          )}
-          {showWmts && (
-            <TileLayer
-              source={wmts({
-                url: 'http://opencache.statkart.no/gatekeeper/gk/gk.open_wmts?',
-                layer: wmtsLayer,
-                matrixSet: sProjection,
-                projection: projection,
-                tileGrid: tileGrid,
-                style: 'default',
-                format: 'image/png',
-              })}
-              zIndex={0}
-            />
-          )}
-          {showMarker && <VectorLayer source={vector({ features })} style={FeatureStyles.MultiPolygon} zIndex={0} />}
-        </Layers>
+        {layerSelection}
         {/* <Controls> */}
         {/* <FullScreenControl /> */}
         {/* </Controls> */}
       </Map>
       <div className="overlayLayer">
-        <h1>Norgeskart</h1>
-        <SearchInput />
-
         <div>
-          <input type="checkbox" checked={showOsm} onChange={event => setShowOsm(event.target.checked)} /> OSM
+          <input type="checkbox" checked={showOsm} onChange={handleCheckboxOsm} /> OSM
         </div>
         <div>
-          <input type="checkbox" checked={showWmts} onChange={event => setShowWmts(event.target.checked)} /> WMTS
-          {!showWmts && (
-            <select
-              value={wmtsLayer}
-              onChange={event => setWmtsLayer(event.target.value)}
-            >
-              {wmtsLayers.map(l => (
-                <option key={l.value} value={l.value}>
-                  {l.label}
+          <input type="checkbox" checked={showWmts} onChange={handleCheckboxWmts} /> WMTS
+          {showWmts && (
+            <select value={wmtsLayer} onChange={handleSetWmtsLayer}>
+              {wmtsLayers.map(selectedLayer => (
+                <option key={selectedLayer.value} value={selectedLayer.value}>
+                  {selectedLayer.label}
                 </option>
               ))}
             </select>
@@ -140,16 +150,13 @@ export default function OverLayLayer() {
         </div>
 
         <div>
-          <input type="checkbox" checked={showLayer1} onChange={event => setShowLayer1(event.target.checked)} /> Ringve
-          botaniske
+          <input type="checkbox" checked={showLayer1} onChange={handleShowLayer1} /> Ringve botaniske
         </div>
         <div>
-          <input type="checkbox" checked={showLayer2} onChange={event => setShowLayer2(event.target.checked)} />{' '}
-          Grillstad marina
+          <input type="checkbox" checked={showLayer2} onChange={handleShowLayer2} /> Grillstad marina
         </div>
         <div>
-          <input type="checkbox" checked={showMarker} onChange={event => setShowMarker(event.target.checked)} /> Show
-          markers
+          <input type="checkbox" checked={showMarker} onChange={handleShowMarkers} /> Show markers
         </div>
         <Logo />
       </div>
