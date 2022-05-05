0ec47dec6f3c0c4025ed445734054bd903c88631
Magne Tøndel <magne.tondel@norconsult.com>
2022-05-05 15:20:19 +0200

    NOR-166: Change base layer component - only layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import ChangeBaseLayer from './components/change-base-layer/ChangeBaseLayer';
 import MainMap from './components/MainMap';
 
 function App() {
   return (
     <div className="App">
       <MainMap />
+      <ChangeBaseLayer />
     </div>
   );
 }
