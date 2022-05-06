0b7f96c7acf47901f4bd887a2af795006bcf83f4
Knud S. Knudsen <Knud.Knudsen@norconsult.com>
2022-05-06 14:33:33 +0200

    removed search to separate branch

--- src/components/TopLeftMenu.tsx
+++ src/components/TopLeftMenu.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import Search from './search/Search';
 
 const TopLeftMenu = () => {
   const { t, i18n } = useTranslation();
@@ -15,7 +14,6 @@ const TopLeftMenu = () => {
         </h1>
       </div>
       {/* <Trans i18nKey="description.part1" />  */}
-      <Search />
     </>
   );
 };
