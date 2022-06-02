00cd4efb8f88287312bc7e7d6673f247347e70b0
Juraj Cirbus <Juraj.Cirbus@norconsult.com>
2022-06-02 09:25:25 +0200

    -remove maplib

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import ChangeBaseLayer from './components/change-base-layer/ChangeBaseLayer';
 import MainMenuPanel from './components/main-menu-panel/MainMenuPanel';
-import MainMap from './components/MainMap';
 import MapTest from './components/MapTest';
 import Logo from './components/Logo';
 import Search from './components/search/Search';
