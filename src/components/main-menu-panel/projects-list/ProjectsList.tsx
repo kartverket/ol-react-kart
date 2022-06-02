1850ba803a06cf5bd65d8be8564aa0d548f27f79
Amund Nedland <Amund.Nedland@norconsult.com>
2022-06-02 15:11:29 +0200

    Translation

--- src/components/main-menu-panel/projects-list/ProjectsList.tsx
+++ src/components/main-menu-panel/projects-list/ProjectsList.tsx
@@ -3,9 +3,12 @@ import { faMap, faChevronUp, faChevronDown } from '@fortawesome/free-solid-svg-i
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useAppSelector, useAppDispatch } from '../../../index';
 import { selectProjectsList, showActiveProjectFromList } from './projectsListSlice';
+import { useTranslation } from 'react-i18next';
 
 
 const ProjectsList = () => {
+  const { t } = useTranslation();
+
   const listProjects = useAppSelector(selectProjectsList);
   const dispatch = useAppDispatch();
 
@@ -18,7 +21,7 @@ const ProjectsList = () => {
       <ul className="list-group list-group-flush">
         {listProjects.map((project, index) => 
           <li key={index} className="list-group-item pt-2 pb-2 text-capitalize" onClick={() => toggleShowActiveProjectFromList()}>
-            {project.ProjectName}
+            {t(project.ProjectName)}
           </li>
         )}
       </ul>
