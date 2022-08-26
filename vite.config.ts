b95dc954987189c2248842cb112c3517005d6190
Carsten Mielke <carsten.mielke@kartverket.no>
2022-08-26 10:19:04 +0200

    fix vite relaoding isssue

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,19 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({
+    include: '**.*(ts|tsx)',
+
+  })],
   base: './',
   build: {
     outDir: 'build',
+  },
+  server: {
+    hmr: true,
+    watch: {
+      usePolling: true,
+    },
+    port: 3000,
   },
 })
